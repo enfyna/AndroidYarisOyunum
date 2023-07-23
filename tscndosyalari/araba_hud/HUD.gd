33f068e0ee9586efb79fea27ccbc5723b3be5415
enfyna <legendraslan@gmail.com>
2023-07-23 21:47:24 +0300

    Add HUD

--- tscndosyalari/araba_hud/HUD.gd
+++ tscndosyalari/araba_hud/HUD.gd
@@ -1,14 +1,30 @@
 class_name HUD
 extends Control
 
-@export var gauges : Dictionary = {
-	"throttle":null,
-	"brake":null,
-	"clutch":null,
-	"rpm":null,
-	"steering":null,
-	"gear":null,
-	"speed":null,
-	
+enum GAUGE_TYPE {
+	FRAME, # gauges that need to update every frame
+	LAP, # gauges that need to update every lap
+	SPECIAL, # gauges that should be updated with special signals
 }
 
+# ^"" -> NodePath Literal
+@export var gauges : Dictionary = {
+	GAUGE_TYPE.FRAME:{
+		Car.STATE.THROTTLE:^"THROTTLE",
+		Car.STATE.BRAKE:^"BRAKE",
+		Car.STATE.CLUTCH:^"CLUTCH",
+		Car.STATE.STEER:^"STEER",
+		Car.STATE.ENGINE_RPM:^"ENGINE_RPM",
+		Car.STATE.SPEED_MPS:^"SPEED_MPS",
+	},
+	GAUGE_TYPE.LAP:{
+		Car.STATE.WHEEL_LIFE:^"WHEEL_LIFE",
+	},
+	GAUGE_TYPE.SPECIAL:{
+		Car.STATE.GEAR:^"GEAR",
+		Car.STATE.ABS:^"ABS",
+		Car.STATE.OIL:^"OIL",
+		Car.STATE.ENGINE:^"ENGINE",
+		Car.STATE.WHEEL_TYPE:^"WHEEL_TYPE",
+	},
+}
